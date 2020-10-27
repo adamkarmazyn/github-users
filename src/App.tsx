2f9a9e195a2fb7c2bad0584241c1cdb5623deffc
bmxadka <akarmazyn@pl.sii.eu>
2020-10-27 16:38:16 +0100

    config

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { PageNotFound } from './components';
 
-function App() {
+import './App.css';
+import Home from './Home';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/te">
+            <div>test</div>
+          </Route>
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
